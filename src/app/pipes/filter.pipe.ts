56e4474bb7770980d8bb9f6404817c8ea6bb834d
Malika Arora <malika.arora@citrix.com>
2023-02-26 18:53:30 +0530

    Search & filter done

--- src/app/pipes/filter.pipe.ts
+++ src/app/pipes/filter.pipe.ts
@@ -5,13 +5,33 @@ import { Pipe, PipeTransform } from "@angular/core";
 })
 
 export class FilterPipe implements PipeTransform {
-  transform(value: any, args?: any): any {
+  // transform(value: any, args?: any): any {
+  //   if (!value) return null;
+  //   if (!args) return value;
+
+  //   args = args.toLowerCase(); debugger;
+  //   return value.filter(function (item) {
+  //     return JSON.stringify(item)
+  //       .toLowerCase()
+  //       .includes(args);
+  //   });
+  // }
+
+  transform(values: any, args?: any[]): any[] {
+    let res = values.filter(a => {
+      return args.length ? args.indexOf(a.status) != -1 : values;
+    })      
+    console.log("Res ", res);
+    return res;
+  }
+
+  transformvalue(value: any, key:any, args?: any): any {
     if (!value) return null;
     if (!args) return value;
 
     args = args.toLowerCase(); debugger;
     return value.filter(function (item) {
-      return JSON.stringify(item)
+      return JSON.stringify(item.key)
         .toLowerCase()
         .includes(args);
     });

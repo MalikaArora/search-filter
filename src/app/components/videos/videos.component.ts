c7dc8ad80b0857bf0715f07eb7360b2d3ecbc18f
Malika Arora <malika.arora@citrix.com>
2023-03-20 00:13:00 +0530

    video fix

--- src/app/components/videos/videos.component.ts
+++ src/app/components/videos/videos.component.ts
@@ -7,17 +7,17 @@ import { Component } from '@angular/core';
 })
 export class VideosComponent {
   openVideo1() {
-    window.open('../../../assets/videos/01 Power of Telematics.mp4', '_blank');
+    window.open('https://drive.google.com/file/d/1SMv7vEid5DwFuxcNmI9prLS4LqyD1DGo/view', '_blank');
   }
 
   openVideo2() {
-    window.open('../../../assets/videos/02 SiteWatch.mp4', '_blank');
+    window.open('https://drive.google.com/file/d/1t-pPgdmkriNL7gS0WiRSR65rmkAbopsJ/view', '_blank');
   }
 
   openVideo3() {
-    window.open('../../../assets/videos/03 SiteConnect.mp4', '_blank');
+    window.open('https://drive.google.com/file/d/1Dc9SFEldnBT1eUYHo9ekGgXvcP1vLCmn/view', '_blank');
   }
   openVideo4() {
-    window.open('../../../assets/videos/04 Customer Testmonials.mp4', '_blank');
+    window.open('https://drive.google.com/file/d/1v55EhmOZT598FBHTLxBrOVFJOsbIxvro/view', '_blank');
   }
 }

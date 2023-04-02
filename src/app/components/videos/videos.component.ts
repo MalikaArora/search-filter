90583774c2a4557c3eaf777bd05d52b70ae69de4
TavinderArora <tpsarora@gmail.com>
2023-04-02 15:09:05 +0530

    02042023-9

--- src/app/components/videos/videos.component.ts
+++ src/app/components/videos/videos.component.ts
@@ -10,14 +10,25 @@ export class VideosComponent {
     window.open('https://drive.google.com/file/d/1SMv7vEid5DwFuxcNmI9prLS4LqyD1DGo/view', '_blank');
   }
 
-  openVideo2() {
+  openVideo2a() {
     window.open('https://drive.google.com/file/d/1t-pPgdmkriNL7gS0WiRSR65rmkAbopsJ/view', '_blank');
   }
-
-  openVideo3() {
+  openVideo2b() {
+    window.open('https://drive.google.com/file/d/13Ke1yYQb4-UUTZlLETGUjPFD5B5TBOiu/view?usp=share_link]/view', '_blank');
+  }
+  openVideo2c() {
+    window.open('https://drive.google.com/file/d/1U3I09NZJWZuErtFylCVwR4ZoVmIwp7QA/view?usp=share_linkJ/view', '_blank');
+  }
+  openVideo3a() {
     window.open('https://drive.google.com/file/d/1Dc9SFEldnBT1eUYHo9ekGgXvcP1vLCmn/view', '_blank');
   }
-  openVideo4() {
+  openVideo3b() {
+    window.open('https://drive.google.com/file/d/1989otqbNzH63tzOLff5kv4wXu-9gdi1V/view?usp=share_link/view', '_blank');
+  }
+  openVideo4a() {
     window.open('https://drive.google.com/file/d/1v55EhmOZT598FBHTLxBrOVFJOsbIxvro/view', '_blank');
+  }
+  openVideo4b() {
+    window.open('https://drive.google.com/file/d/1r4WuZC52Af-whFKweDLZkcEmJcPeEURK/view?usp=share_link/view', '_blank');
   }
 }

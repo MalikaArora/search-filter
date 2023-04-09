859664ebb509108327dc693c3d9604cc4eca736a
Malika Arora <malika.arora@citrix.com>
2023-04-09 22:34:31 +0530

    videos fix

--- src/app/components/videos/videos.component.ts
+++ src/app/components/videos/videos.component.ts
@@ -14,21 +14,21 @@ export class VideosComponent {
     window.open('https://drive.google.com/file/d/1t-pPgdmkriNL7gS0WiRSR65rmkAbopsJ/view', '_blank');
   }
   openVideo2b() {
-    window.open('https://drive.google.com/file/d/13Ke1yYQb4-UUTZlLETGUjPFD5B5TBOiu/view?usp=share_link]/view', '_blank');
+    window.open('https://drive.google.com/file/d/13Ke1yYQb4-UUTZlLETGUjPFD5B5TBOiu/view', '_blank');
   }
   openVideo2c() {
-    window.open('https://drive.google.com/file/d/1U3I09NZJWZuErtFylCVwR4ZoVmIwp7QA/view?usp=share_linkJ/view', '_blank');
+    window.open('https://drive.google.com/file/d/1U3I09NZJWZuErtFylCVwR4ZoVmIwp7QA/view', '_blank');
   }
   openVideo3a() {
     window.open('https://drive.google.com/file/d/1Dc9SFEldnBT1eUYHo9ekGgXvcP1vLCmn/view', '_blank');
   }
   openVideo3b() {
-    window.open('https://drive.google.com/file/d/1989otqbNzH63tzOLff5kv4wXu-9gdi1V/view?usp=share_link/view', '_blank');
+    window.open('https://drive.google.com/file/d/1989otqbNzH63tzOLff5kv4wXu-9gdi1V/view', '_blank');
   }
   openVideo4a() {
     window.open('https://drive.google.com/file/d/1v55EhmOZT598FBHTLxBrOVFJOsbIxvro/view', '_blank');
   }
   openVideo4b() {
-    window.open('https://drive.google.com/file/d/1r4WuZC52Af-whFKweDLZkcEmJcPeEURK/view?usp=share_link/view', '_blank');
+    window.open('https://drive.google.com/file/d/1r4WuZC52Af-whFKweDLZkcEmJcPeEURK/view', '_blank');
   }
 }
